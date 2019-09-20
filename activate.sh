94a24e222803b681c782823976a47ac0615cfd11
Joseph Yu <joseph.yu@wwfx.co.uk>
2019-09-20 20:24:28 +0100

    First test run

--- activate.sh
+++ activate.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-set -eux -o pipefail
+set -eu -o pipefail
 
-echo "Oh hi there!"
-env | sort
+cd /etc/yum.repos.d/
+echo "Extracting into $(pwd)..."
+curl -sL https://github.com/wwfxuk/centos-vault-scl/archive/${GITHUB_SHA:-master}.tar.gz \
+    | tar -v --strip-components=1 -xz '*.repo'
+ls -lah
+
+CENTOS_RELEASE=$(grep -oP '[\d\.]+' /etc/centos-release)
+
+echo "Enabling C${CENTOS_RELEASE}-centos-sclo-sclo..."
+yum-config-manager --enable C${CENTOS_RELEASE}-centos-sclo-sclo | grep -q '^enabled = '
+
+echo "Enabling C${CENTOS_RELEASE}-centos-sclo-rh..."
+yum-config-manager --enable C${CENTOS_RELEASE}-centos-sclo-rh | grep -q '^enabled = '
+
+
+echo "Disabling centos-sclo-sclo..."
+yum-config-manager --disable centos-sclo-sclo | grep -q '^enabled = '
+
+
+echo "Disabling centos-sclo-rh..."
+yum-config-manager --disable centos-sclo-rh | grep -q '^enabled = '
+
+echo "========================================================================"
+echo "You shoud be able to install packages like devtoolset-3 now!"
