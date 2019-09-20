f0f7384f643fb01fb4bc09543cd02f6ddbb6ebde
Joseph Yu <joseph.yu@wwfx.co.uk>
2019-09-20 20:34:57 +0100

    Added catch for missing tar binary

--- activate.sh
+++ activate.sh
@@ -2,9 +2,12 @@
 set -eu -o pipefail
 
 cd /etc/yum.repos.d/
-echo "Extracting into $(pwd)..."
-curl -sL https://github.com/wwfxuk/centos-vault-scl/archive/${GITHUB_SHA:-master}.tar.gz \
-    | tar -v --strip-components=1 -xz '*.repo'
+CENTOS_VAULT_SCL_TAR="https://github.com/wwfxuk/centos-vault-scl/archive/${GITHUB_SHA:-master}.tar.gz"
+command -v tar || ( echo "Installing tar..." && yum install -y tar )
+
+echo "Extracting *.repo files from ${CENTOS_VAULT_SCL_TAR}"
+echo "into $(pwd)..."
+curl -sL "${CENTOS_VAULT_SCL_TAR}" | tar -v --strip-components=1 -xz '*.repo'
 ls -lah
 
 CENTOS_RELEASE=$(grep -oP '[\d\.]+' /etc/centos-release)
