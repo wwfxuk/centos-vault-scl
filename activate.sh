bc3d28c3ecbb54a607fa38a471853a36084faa64
Joseph Yu <joseph.yu@wwfx.co.uk>
2019-09-20 21:01:55 +0100

    Fixed activate deps, updated repos

--- activate.sh
+++ activate.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+
+#
+# usage: activate.sh
+#
+# Fetch and activate vault/archive sclo repositories for current CentOS 7
+# version. Will also download required dependencies if missing:
+#
+# - tar, e.g. missing on CentOS 7.1
+# - yum-config-manager, e.g. missing on CentOS 7.1
+#
+# If any of the required dependencies are installed, curl will also be updated
+# so it can also pull/download from GitHub (more modern NSS).
+#
+# ENVIRONMENT VARIABLES
+#
+#   GITHUB_SHA: Custom commit SHA to pull from GitHub,
+#               instead of the default master branch.
+#
 set -eu -o pipefail
 
 cd /etc/yum.repos.d/
 CENTOS_VAULT_SCL_TAR="https://github.com/wwfxuk/centos-vault-scl/archive/${GITHUB_SHA:-master}.tar.gz"
-command -v tar || ( echo "Installing tar..." && yum install -y tar )
+
+DEPS_NEEDED=""
+command -v tar || DEPS_NEEDED+=" tar"
+command -v yum-config-manager || DEPS_NEEDED+=" yum-utils"
+test -z "${DEPS_NEEDED}" || ( echo "Installing dependencies and updating curl..." && yum install -y $DEPS_NEEDED curl)
 
 echo "Extracting *.repo files from ${CENTOS_VAULT_SCL_TAR}"
 echo "into $(pwd)..."
@@ -26,5 +48,5 @@ yum-config-manager --disable centos-sclo-sclo | grep -q '^enabled = '
 echo "Disabling centos-sclo-rh..."
 yum-config-manager --disable centos-sclo-rh | grep -q '^enabled = '
 
-echo "========================================================================"
-echo "You shoud be able to install packages like devtoolset-3 now!"
+echo '========================================================================'
+echo 'You shoud be able to install packages like devtoolset-3 now!'
